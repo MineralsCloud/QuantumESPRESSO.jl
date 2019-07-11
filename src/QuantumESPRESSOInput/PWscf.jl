d2d37a0507403444a38a91b0f56a6e4d3024617a
Qi Zhang <singularitti@outlook.com>
2019-07-11 18:33:08 -0400

    Add assertions to `PWscfInput`

--- src/QuantumESPRESSOInput/PWscf.jl
+++ src/QuantumESPRESSOInput/PWscf.jl
@@ -13,13 +13,25 @@ module PWscf
 
 using Parameters: @with_kw
 
+using QuantumESPRESSO.Namelists.PWscf
+using QuantumESPRESSO.Cards.PWscf
 using QuantumESPRESSO.QuantumESPRESSOInput
 
 export PWscfInput
 
 @with_kw struct PWscfInput <: Input
-    namelists::Dict
-    cards::Dict
+    namelists::Dict; @assert all([
+        isempty(setdiff([:CONTROL, :SYSTEM, :ELECTRONS], keys(namelists))),
+        isa(namelists[:CONTROL], ControlNamelist),
+        isa(namelists[:SYSTEM], SystemNamelist),
+        isa(namelists[:ELECTRONS], ElectronsNamelist)
+    ])
+    cards::Dict; @assert all([
+        isempty(setdiff([:ATOMIC_SPECIES, :ATOMIC_POSITIONS, :K_POINTS], keys(cards))),
+        isa(cards[:ATOMIC_SPECIES], AtomicSpeciesCard),
+        isa(cards[:ATOMIC_POSITIONS], AtomicPositionCard),
+        isa(cards[:K_POINTS], KPointsCard)
+    ])
 end  # struct PWscfInput
 
 end
