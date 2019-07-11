55cfe173d02b9174d9ddfdea05b8f862ec99a46f
Qi Zhang <singularitti@outlook.com>
2019-07-11 14:04:08 -0400

    Fix repo organization

--- docs/make.jl
+++ docs/make.jl
@@ -6,12 +6,12 @@ makedocs(;
     pages=[
         "Home" => "index.md",
     ],
-    repo="https://github.com/singularitti/QuantumESPRESSO.jl/blob/{commit}{path}#L{line}",
+    repo="https://github.com/MineralsCloud/QuantumESPRESSO.jl/blob/{commit}{path}#L{line}",
     sitename="QuantumESPRESSO.jl",
     authors="Qi Zhang <singularitti@outlook.com>",
     assets=String[],
 )
 
 deploydocs(;
-    repo="github.com/singularitti/QuantumESPRESSO.jl",
+    repo="github.com/MineralsCloud/QuantumESPRESSO.jl",
 )
