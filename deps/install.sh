0b7402361097833e8c6e68dae78783a70fccedf8
Qi Zhang <singularitti@outlook.com>
2019-07-17 00:13:05 -0400

    Remove the using of `f90nml`

--- deps/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-virtualenv f90nml
-
-source f90nml/bin/activate
-
-pip install f90nml
-
-deactivate
--- src/Readers/NamelistReaders.jl
+++ src/Readers/NamelistReaders.jl
@@ -11,13 +11,6 @@ julia>
 """
 module NamelistReaders
 
-ENV["PYTHON"] = joinpath(@__FILE__, "../../deps/f90nml/bin/python")
-ENV["PYCALL_JL_RUNTIME_PYTHON"] = joinpath(@__FILE__, "../../deps/f90nml/bin/python")
 
-println(Sys.which("python"))
-
-using PyCall
-
-pyimport("f90nml")
 
 end
