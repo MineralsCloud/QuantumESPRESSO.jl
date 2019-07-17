8245f907ac15657b72b0982f3776f606c72db4d5
Qi Zhang <singularitti@outlook.com>
2019-07-17 01:45:52 -0400

    Add function `read_namelist`

--- src/Readers/NamelistReaders.jl
+++ src/Readers/NamelistReaders.jl
@@ -11,6 +11,20 @@ julia>
 """
 module NamelistReaders
 
-
+function read_namelist(io::IOStream)
+    result = Dict()
+    for line in eachline(io)  # Read each line in the namelist until '/'
+        s = strip(line)
+        # Use '=' as the delimiter, split the stripped line into a key and a value.
+        # Skip this line if a line starts with '&' (namelist caption) or '!' (comment) or
+        # this line is empty ('').
+        startswith(s, '&') || startswith(s, '!') or isempty(s) && continue
+        k, v = split(s, '=')  # FIXME: maxsplit=1
+        k = strip(k)
+        v = split(strip(rstrip(',', strip(v))), '!') |> first  # Ignore trailing comma of the line
+        push!(result[k],v)
+    end
+    return result
+end  # function read_namelist
 
 end
