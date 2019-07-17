acde2a43828ca2c61913e2e4be1bfec91c248340
Qi Zhang <singularitti@outlook.com>
2019-07-17 00:59:38 -0400

    Add `read_atomicpositions` function

--- src/Readers/CardReaders.jl
+++ src/Readers/CardReaders.jl
@@ -29,4 +29,39 @@ function read_atomicspecies(io::IOStream)
     return AtomicSpeciesCard(data = atomic_species)
 end  # function read_atomicspecies
 
+function read_atomicpositions(io::IOStream)
+    atomic_positions = []
+    title_line = io[1]
+    m = match(r"ATOMIC_POSITIONS\s*(?:[({])?\s*(\w*)\s*(?:[)}])?", title_line, flags = re.IGNORECASE)
+    m === nothing && error("No match found in the line '$(title_line)'! Something went wrong!")
+    option = m.captures[2]
+    if isempty(option)
+        @warn "No option is found, default option 'alat' will be set! " *
+              "Not specifying units is DEPRECATED and will no longer be allowed in the future"
+        option = "alat"
+    end
+    for line in s[2:end]
+        # If this line is an empty line or a line of comment.
+        isempty(line) || startswith(strip(line), '!') && continue
+        if strip(line) == '/'
+            error("Do not start any line in cards with a '/' character!")
+        end
+        if match(r"\\{.*\\}", line):
+            m = match(r"(\w+)\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)\s*\\{\s*([01])?\s*([01])?\s*([01])?\s*\\}",
+                strip(line))
+            name, x, y, z, if_pos1, if_pos2, if_pos3 = m.captures
+            push!(atomic_positions, AtomicPosition(atom = name, position = [x, y, z]))
+        else
+            m = match("(\w+)\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)\s*(-?\d+\.\d+)", strip(line))
+            if m === nothing
+                @warn "No match found in the line $(line)!"
+            else
+                name, x, y, z = m.captures
+                push!(atomic_positions, AtomicPosition(atom = name, position = [x, y, z]))
+            end
+        end
+    end
+    return AtomicPositionCard(option = option, data = atomic_positions)
+end  # function read_atomicpositions
+
 end
