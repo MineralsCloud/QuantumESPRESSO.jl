cfe0fa8ffbeaf0ba014de038bc232835cc784a22
Qi Zhang <singularitti@outlook.com>
2019-07-17 20:56:07 -0400

    Rewrite `get_namelist_identifier_positions`

--- src/Readers/splitting.jl
+++ src/Readers/splitting.jl
@@ -4,23 +4,19 @@ splitting:
 - Author: singularitti
 - Date: 2019-07-17
 =#
+using DataStructures: SortedDict
+
 const NAMELIST_SEP = r"/\s*[\r\n]"
 const NAMELIST_IDENTIFIERS = r"&CONTROL"i, r"&SYSTEM"i, r"&ELECTRONS"i, r"&IONS"i, r"&CELL"i
 const CARD_IDENTIFIERS = r"ATOMIC_SPECIES"i, r"ATOMIC_POSITIONS"i, r"K_POINTS"i, r"CELL_PARAMETERS"i, r"OCCUPATIONS"i, r"CONSTRAINTS"i, r"ATOMIC_FORCES"i
 
-struct RangeIndices
-    startindex
-    endindex
-end  # struct RangeIndices
-
-function get_namelist_identifier_positions(io::IOStream; include_heading::Bool=true, include_ending::Bool=false)
+function get_namelist_identifier_positions(io::IOStream)
     match_records = Dict()
     for pattern in NAMELIST_IDENTIFIERS
-        m0 = findfirst(pattern, io)
+        m0 = findall(str -> occursin(pattern, str), io)
         m0 === nothing && continue
-        m1 = findnext(NAMELIST_SEP, io)
-        match_records[pattern] = RangeIndices(startindex=(include_heading ? m0.start() : m0.end()), endindex=(include_ending ? m1.end() : m1.start()))
+        m1 = findnext(str -> occursin(NAMELIST_SEP, str), io, m0)
+        match_records[pattern] = range(m0, m1)
     end
-    m = sorted(match_records.items(), key=operator.itemgetter(1))
-    return OrderedDict(m)
+    return SortedDict(sort(collect(match_records), by=x->x[2]))
 end  # function get_namelist_identifier_positions
