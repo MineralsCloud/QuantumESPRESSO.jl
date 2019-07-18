8e52f15f3210f19f6e2f5f401f28ae00bcff4e7e
Qi Zhang <singularitti@outlook.com>
2019-07-17 20:14:36 -0400

    Change `get_namelist_identifier_positions`

--- src/Readers/splitting.jl
+++ src/Readers/splitting.jl
@@ -4,32 +4,23 @@ splitting:
 - Author: singularitti
 - Date: 2019-07-17
 =#
-const NAMELIST_IDENTIFIERS = "&CONTROL", "&SYSTEM", "&ELECTRONS", "&IONS", "&CELL"
-const CARD_IDENTIFIERS = "ATOMIC_SPECIES", "ATOMIC_POSITIONS", "K_POINTS", "CELL_PARAMETERS", "OCCUPATIONS", "CONSTRAINTS", "ATOMIC_FORCES"
+const NAMELIST_SEP = r"/\s*[\r\n]"
+const NAMELIST_IDENTIFIERS = r"&CONTROL"i, r"&SYSTEM"i, r"&ELECTRONS"i, r"&IONS"i, r"&CELL"i
+const CARD_IDENTIFIERS = r"ATOMIC_SPECIES"i, r"ATOMIC_POSITIONS"i, r"K_POINTS"i, r"CELL_PARAMETERS"i, r"OCCUPATIONS"i, r"CONSTRAINTS"i, r"ATOMIC_FORCES"i
 
 struct RangeIndices
     startindex
     endindex
 end  # struct RangeIndices
 
-function get_namelist_identifier_positions(io::IOStream)
+function get_namelist_identifier_positions(io::IOStream; include_heading::Bool=true, include_ending::Bool=false)
     match_records = Dict()
-    identifiers = NAMELIST_IDENTIFIERS
-    for pattern in identifiers
-        # ``re.compile`` will produce a regular expression object, on which we can use its ``search`` method.
-        m0 = re.compile(pattern, flags=re.IGNORECASE).search(io, match_records.get(pattern, pos))
+    for pattern in NAMELIST_IDENTIFIERS
+        m0 = findfirst(pattern, io)
         m0 === nothing && continue
-        m1 = re.compile(self.namelist_sep, flags=re.IGNORECASE).search(io, m0.end())
-        match_records[pattern] = RangeIndices(startindex={True: m0.start(), False: m0.end()}[include_heading], endindex={True: m1.end(), False: m1.start()}[include_ending])
+        m1 = findnext(NAMELIST_SEP, io)
+        match_records[pattern] = RangeIndices(startindex=(include_heading ? m0.start() : m0.end()), endindex=(include_ending ? m1.end() : m1.start()))
     end
-    # The following one-line code first sort the ``dict`` *positions* by its values, i.e., a ``RangeIndices`` tuple.
-    # Then we get a list of tuples, with first entries to be the identifiers and second indices to be
-    # the indices.
-    # For example, if ``x = {'a': 2, 'b': 3, 'c': 1}``, then
-    # >>> sorted(x.items(), key=operator.itemgetter(1))
-    # [('c', 1), ('a', 2), ('b', 3)]
-    # Tuples are compared lexicographically using comparison of corresponding elements, thus compared with their
-    # *begin* entry.
     m = sorted(match_records.items(), key=operator.itemgetter(1))
     return OrderedDict(m)
 end  # function get_namelist_identifier_positions
