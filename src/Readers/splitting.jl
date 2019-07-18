48da629b128c8d22db52a15ce48f20d36ed3c3d0
Qi Zhang <singularitti@outlook.com>
2019-07-18 03:18:40 -0400

    Implement `get_card_identifier_indices` in a simple way

--- src/Readers/splitting.jl
+++ src/Readers/splitting.jl
@@ -4,13 +4,25 @@ splitting:
 - Author: singularitti
 - Date: 2019-07-17
 =#
-using DataStructures: SortedDict
+using DataStructures: SortedDict, OrderedDict
+using IterUtils: throw_which_occursin
 
 export get_namelist_identifier_indices
 
 const NAMELIST_END = raw"/\s*[\r\n]"
 const NAMELIST_STARTS = "&CONTROL", "&SYSTEM", "&ELECTRONS", "&IONS", "&CELL"
 const CARD_STARTS = "ATOMIC_SPECIES", "ATOMIC_POSITIONS", "K_POINTS", "CELL_PARAMETERS", "OCCUPATIONS", "CONSTRAINTS", "ATOMIC_FORCES"
+
+function get_card_identifier_indices(io::IOStream)
+    records = OrderedDict()
+    for (i, line) in enumerate(eachline(io))
+        str = strip(line)
+        isempty(str) || startswith(str, r"[!#]") && continue
+        cardname = throw_which_occursin(CARD_STARTS, str)
+        cardname === nothing ? continue : records[cardname] = i
+    end  # for
+    return records
+end  # function get_namelist_identifier_indices
 
 function get_namelist_identifier_indices(io::IOStream)
     match_records = Dict()
