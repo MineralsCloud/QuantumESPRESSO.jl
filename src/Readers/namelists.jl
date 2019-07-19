fa5c90f5da6e1a7f2a4fa6f759664bb47211ff46
Qi Zhang <singularitti@outlook.com>
2019-07-18 23:15:55 -0400

    Simplify code `read_namelist`

--- src/Readers/namelists.jl
+++ src/Readers/namelists.jl
@@ -9,12 +9,12 @@ export read_namelist
 function read_namelist(lines)
     result = Dict()
     for line in lines
-        s = strip(line)
+        str = strip(line)
         # Use '=' as the delimiter, split the stripped line into a key and a value.
         # Skip this line if a line starts with '&' (namelist caption) or '!' (comment) or
         # this line is empty ('').
-        (startswith(s, '&') || startswith(s, '!') || startswith(s, '/') || isempty(s)) && continue
-        k, v = split(s, '=', limit=2)
+        isempty(str) || any(startswith(str, x) for x in ('!', '/', '&')) && continue
+        k, v = split(str, '=', limit=2)
         k = strip(k)
         v = first(split(strip(rstrip(strip(v), ',')), '!'))  # Ignore trailing comma of the line
         result[k] = v
