a362cca01829503bddb37b1ead7109f3e75e9808
Qi Zhang <singularitti@outlook.com>
2019-07-20 01:28:28 -0400

    Add method `typefield`

--- src/QuantumESPRESSOInput/PW.jl
+++ src/QuantumESPRESSOInput/PW.jl
@@ -12,6 +12,7 @@ julia>
 module PW
 
 using Parameters: @with_kw
+using Rematch: @match
 
 using QuantumESPRESSO.Namelists
 using QuantumESPRESSO.Namelists.PW
@@ -20,6 +21,7 @@ using QuantumESPRESSO.Cards.PW
 using QuantumESPRESSO.QuantumESPRESSOInput
 
 export PWInput,
+    typefield,
     namelists,
     cards
 
@@ -34,6 +36,25 @@ export PWInput,
     kpoints::KPointsCard
     cellparameters::CellParametersCard
 end  # struct PWInput
+
+function typefield(type::T) where {T <: Union{Namelist, Card}}
+    if T <: Namelist
+        @match T begin
+            ControlNamelist => :control
+            SystemNamelist => :system
+            ElectronsNamelist => :electrons
+            IonsNamelist => :ions
+            CellNamelist => :cell
+        end
+    else  # T <: Card
+        @match T begin
+            AtomicSpeciesCard => :atomicspecies
+            AtomicPositionCard => :atomicpositions
+            KPointsCard => :kpoints
+            CellParametersCard => :cellparameters
+        end
+    end  # if-else
+end  # function typefield
 
 filter_field_by_supertype(obj, ::Type{T}) where {T} = filter(x->isa(x, T), map(x->getfild(ob, x), fieldnames(typeof(obj))) |> collect)
 
