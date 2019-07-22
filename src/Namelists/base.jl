650072e96dd39b47a825b9d9ecc3093b1e83700f
Qi Zhang <singularitti@outlook.com>
2019-07-21 21:00:42 -0400

    Add `to_dict` method to `Namelist`s

--- src/Namelists/base.jl
+++ src/Namelists/base.jl
@@ -4,6 +4,12 @@ base:
 - Author: singularitti
 - Date: 2019-07-21
 =#
+using Parameters: type2dict
+
 export Namelist
 
 abstract type Namelist end
+
+function to_dict(nml::Namelist)::Dict{Symbol,Any}
+    return type2dict(nml)
+end  # function to_dict
