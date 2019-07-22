43730b98188433a98a9c3dec678fc4da749d0c2b
Qi Zhang <singularitti@outlook.com>
2019-07-21 21:06:29 -0400

    Add `evolve` method to `Namelist`

--- src/Namelists/base.jl
+++ src/Namelists/base.jl
@@ -4,7 +4,7 @@ base:
 - Author: singularitti
 - Date: 2019-07-21
 =#
-using Parameters: type2dict
+using Parameters: type2dict, reconstruct
 
 export Namelist
 
@@ -13,3 +13,10 @@ abstract type Namelist end
 function to_dict(nml::Namelist)::Dict{Symbol,Any}
     return type2dict(nml)
 end  # function to_dict
+
+function evolve(nml::T, kwargs...)::T where {T <: Namelist}
+    return reconstruct(nml, kwargs...)
+end  # function evolve
+function evolve(nml::T, dict::AbstractDict{Symbol,Any})::T where {T <: Namelist}
+    return reconstruct(nml, dict)
+end  # function evolve
