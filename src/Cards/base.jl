d0e50fe2565b6bf7f0a1fae60290a249373889e8
Qi Zhang <singularitti@outlook.com>
2019-07-22 06:07:42 -0400

    Add `evolve` for `Card`

--- src/Cards/base.jl
+++ src/Cards/base.jl
@@ -7,7 +7,8 @@ base:
 export Card,
     name,
     option,
-    allowed_options
+    allowed_options,
+    evolve
 
 abstract type Card end
 
@@ -16,3 +17,11 @@ name(::Type{<: Card}) = error("Undefined name!")
 option(card::Card) = getfield(card, :option)
 
 allowed_options(::Type{<: Card}) = error("No allowed options defined!")
+
+function evolve(card::Card, newdict::AbstractDict)
+    :option in keys && error("If you want to change the option of a card, reconstruct a new one!")
+    return reconstruct(card, newdict)
+end  # function evolve
+function evolve(card::Card; kwargs...)
+    return evolve(card, kwargs)
+end  # function evolve
