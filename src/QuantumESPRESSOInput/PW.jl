a5755a28dab752f7f1bcfe0ef09d665ed91acdc9
Qi Zhang <singularitti@outlook.com>
2019-07-22 00:52:55 -0400

    Deprecate `typefield`
    We already have `name`, they do the same thing.
    But `name` is user-friendlier and robuster.

--- src/QuantumESPRESSOInput/PW.jl
+++ src/QuantumESPRESSOInput/PW.jl
@@ -20,7 +20,6 @@ using QuantumESPRESSO.Cards.PW
 using QuantumESPRESSO.QuantumESPRESSOInput
 
 export PWInput,
-    typefield,
     namelists,
     cards
 
@@ -35,16 +34,6 @@ export PWInput,
     kpoints::KPointsCard
     cellparameters::CellParametersCard
 end  # struct PWInput
-
-function typefield(type::Type{T}) where {T <: Union{Namelist, Card}}
-    if T <: Namelist
-        index = findfirst([T <: X for X in (ControlNamelist, SystemNamelist, ElectronsNamelist, IonsNamelist, CellNamelist)])
-        return [:control, :system, :electrons, :ions, :cell][index]
-    else  # T <: Card
-        index = findfirst([T <: X for X in (AtomicSpeciesCard, AtomicPositionsCard, KPointsCard, CellParametersCard)])
-        return [:atomicspecies, :atomicpositions, :kpoints, :cellparameters][index]
-    end  # if-else
-end  # function typefield
 
 filter_field_by_supertype(obj, ::Type{T}) where {T} = filter(x->isa(x, T), map(x->getfild(ob, x), fieldnames(typeof(obj))) |> collect)
 
