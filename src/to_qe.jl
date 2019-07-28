643287aae0e8b173d80a3a779a729359cb85a28f
Qi Zhang <singularitti@outlook.com>
2019-07-28 15:09:17 -0400

    Fix a typesetting bug

--- src/to_qe.jl
+++ src/to_qe.jl
@@ -76,7 +76,7 @@ end
 function to_qe(card::KPointsCard; indent::AbstractString = "    ", sep::AbstractString = " ")::String
     content = "K_POINTS$(sep){ $(card.option) }\n"
     if card.option in ("gamma", "automatic")
-        content *= "$(indent)$(to_qe(first(card.data)))"
+        content *= "$(indent)$(to_qe(first(card.data)))\n"
     else  # option in ("tpiba", "crystal", "tpiba_b", "crystal_b", "tpiba_c", "crystal_c")
         content *= "$(length(card.data))\n"
         for x in card.data
