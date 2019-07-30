23a5133e0246528ab23b4e2c878b43cfe79e3931
Qi Zhang <singularitti@outlook.com>
2019-07-29 23:51:33 -0400

    Add `write` method to `PWInput`

---
+++ src/BasicIO/write.jl
@@ -0,0 +1,17 @@
+#=
+write:
+- Julia version: 1.0
+- Author: singularitti
+- Date: 2019-07-29
+=#
+using QuantumESPRESSO
+using QuantumESPRESSO.QuantumESPRESSOInput.PW
+
+function Base.write(io::IO, pw::PWInput, debug::Bool = true)
+    write(io, to_qe(pw, debug = debug))
+end
+function Base.write(path::AbstractPath, pw::PWInput, debug::Bool = true)
+    open(path, "r+") do io
+        write(io, pw, debug)
+    end
+end
