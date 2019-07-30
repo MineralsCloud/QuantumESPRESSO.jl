586c453840e362fb48c9e42ac83112564f7f6223
Qi Zhang <singularitti@outlook.com>
2019-07-30 02:13:22 -0400

    Move `Cards` subpackage to QuantumESPRESSOBase.jl

--- src/QuantumESPRESSO.jl
+++ src/QuantumESPRESSO.jl
@@ -5,7 +5,6 @@ export InputEntry
 abstract type InputEntry end
 
 include("Namelists/Namelists.jl")
-include("Cards/Cards.jl")
 include("QuantumESPRESSOInput/QuantumESPRESSOInput.jl")
 include("name.jl")
 include("to_qe.jl")
