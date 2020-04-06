e18f4672cf9f511f8cfad19acd1bd31d3d01a76c
Qi Zhang <singularitti@outlook.com>
2020-04-06 07:36:25 -0400

    Reexport src/QuantumESPRESSO.jl

--- src/QuantumESPRESSO.jl
+++ src/QuantumESPRESSO.jl
@@ -1,11 +1,188 @@
 module QuantumESPRESSO
 
 using Reexport: @reexport
+
 @reexport using QuantumESPRESSOBase
 @reexport using QuantumESPRESSOParsers
 
-using ImportMacros
-@import QuantumESPRESSOBase.Inputs as Inputs
-@import QuantumESPRESSOParsers.Outputs as Outputs
+module Inputs
 
-end # module
+using Reexport: @reexport
+
+@reexport using QuantumESPRESSOBase.Inputs:
+    Namelist,
+    Card,
+    InputEntry,
+    to_dict,
+    dropdefault,
+    getnamelists,
+    getcards,
+    getoption,
+    allowed_options,
+    titleof,
+    qestring
+@reexport using QuantumESPRESSOParsers.Inputs: InvalidInput, InputFile
+
+export InvalidInput,
+    InputFile,
+    to_dict,
+    dropdefault,
+    getnamelists,
+    getcards,
+    getoption,
+    allowed_options,
+    titleof,
+    qestring
+
+module PWscf
+
+using Reexport: @reexport
+
+@reexport using QuantumESPRESSOBase.Inputs.PWscf:
+    ControlNamelist,
+    SystemNamelist,
+    ElectronsNamelist,
+    IonsNamelist,
+    CellNamelist,
+    DosNamelist,
+    BandsNamelist,
+    AtomicSpecies,
+    AtomicSpeciesCard,
+    AtomicPosition,
+    AtomicPositionsCard,
+    CellParametersCard,
+    AtomicForce,
+    AtomicForcesCard,
+    MonkhorstPackGrid,
+    GammaPoint,
+    SpecialKPoint,
+    KPointsCard,
+    PWInput
+@reexport using QuantumESPRESSOParsers.Inputs.PWscf
+
+export ControlNamelist,
+    SystemNamelist,
+    ElectronsNamelist,
+    IonsNamelist,
+    CellNamelist,
+    DosNamelist,
+    BandsNamelist,
+    AtomicSpecies,
+    AtomicSpeciesCard,
+    AtomicPosition,
+    AtomicPositionsCard,
+    CellParametersCard,
+    AtomicForce,
+    AtomicForcesCard,
+    MonkhorstPackGrid,
+    GammaPoint,
+    SpecialKPoint,
+    KPointsCard,
+    PWInput
+
+end # module PWscf
+
+module PHonon
+
+using Reexport: @reexport
+
+@reexport using QuantumESPRESSOBase.Inputs.PHonon:
+    PhNamelist, Q2rNamelist, MatdynNamelist, DynmatNamelist, Q2rInput, DynmatInput
+# PhInput, MatdynInput
+
+export PhNamelist, Q2rNamelist, MatdynNamelist, DynmatNamelist, Q2rInput, DynmatInput
+# PhInput, MatdynInput
+
+end # module PHonon
+
+end # module Inputs
+
+module Outputs
+
+using Reexport: @reexport
+
+@reexport using QuantumESPRESSOParsers.Outputs: SubroutineError, OutputFile
+
+export SubroutineError, OutputFile
+
+module PWscf
+
+using Reexport: @reexport
+
+@reexport using QuantumESPRESSOParsers.Outputs.PWscf:
+    Diagonalization,
+    Preamble,
+    Diagonalization,
+    Davidson,
+    ConjugateGradient,
+    ProjectedPreconditionedConjugateGradient,
+    parse_fft_base_info,
+    parse_symmetries,
+    parse_ibz,
+    parse_stress,
+    parse_iteration_time,
+    parse_bands,
+    parse_all_electron_energy,
+    parse_energy_decomposition,
+    parse_paw_contribution,
+    parse_smearing_energy,
+    parse_version,
+    parse_parallel_info,
+    parse_fft_dimensions,
+    parse_iteration_head,
+    parse_electrons_energies,
+    parse_clock,
+    whatinput,
+    isrelaxed,
+    isjobdone,
+    tryparsefirst,
+    parsefirst,
+    tryparseall,
+    parseall,
+    tryparselast,
+    parselast,
+    tryparsenext,
+    parsenext,
+    tryparsefinal,
+    parsefinal
+
+export Diagonalization,
+    Preamble,
+    Davidson,
+    ConjugateGradient,
+    ProjectedPreconditionedConjugateGradient,
+    parse_fft_base_info,
+    parse_symmetries,
+    parse_ibz,
+    parse_stress,
+    parse_iteration_time,
+    parse_bands,
+    parse_all_electron_energy,
+    parse_energy_decomposition,
+    parse_paw_contribution,
+    parse_smearing_energy,
+    parse_version,
+    parse_parallel_info,
+    parse_fft_dimensions,
+    parse_iteration_head,
+    parse_electrons_energies,
+    parse_clock,
+    whatinput,
+    isrelaxed,
+    isjobdone,
+    tryparsefirst,
+    parsefirst,
+    tryparseall,
+    parseall,
+    tryparselast,
+    parselast,
+    tryparsenext,
+    parsenext,
+    tryparsefinal,
+    parsefinal
+
+end # module PWscf
+
+end # module Outputs
+
+end # module QuantumESPRESSO
