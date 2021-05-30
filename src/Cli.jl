97db4b7ba2d85ff7dd27e5f4749354e2e36c3f14
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-30 03:32:46 -0400

    Fix names in QuantumESPRESSOCommands.jl

--- src/Cli.jl
+++ src/Cli.jl
@@ -1,20 +1,20 @@
 module Cli
 
-using QuantumESPRESSOCli:
-    MpiexecOptions,
-    PwxOptions,
+using QuantumESPRESSOCommands:
+    ParallelizationFlags,
     PwxConfig,
     PhxConfig,
     Q2rxConfig,
     MatdynxConfig,
     QuantumESPRESSOCliConfig,
-    materialize,
+    readconfig,
     pw,
     ph,
     q2r,
     matdyn,
+    dynmat,
     makecmd
 
-export pw, ph, q2r, matdyn
+export pw, ph, q2r, matdyn, dynmat
 
-end # module Cli
+end
