d4f380330157a17eeada85a90aae1ec8c7432285
singularitti <singularitti@users.noreply.github.com>
2022-04-28 00:05:39 +0000

    Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,24 @@
 using QuantumESPRESSO
 using Documenter
 
-DocMeta.setdocmeta!(QuantumESPRESSO, :DocTestSetup, :(using QuantumESPRESSO); recursive=true)
+DocMeta.setdocmeta!(
+    QuantumESPRESSO,
+    :DocTestSetup,
+    :(using QuantumESPRESSO);
+    recursive = true,
+)
 
 makedocs(;
-    modules=[QuantumESPRESSO],
-    authors="Qi Zhang <singularitti@outlook.com>",
-    repo="https://github.com/MineralsCloud/QuantumESPRESSO.jl/blob/{commit}{path}#{line}",
-    sitename="QuantumESPRESSO.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://MineralsCloud.github.io/QuantumESPRESSO.jl",
-        assets=String[],
+    modules = [QuantumESPRESSO],
+    authors = "Qi Zhang <singularitti@outlook.com>",
+    repo = "https://github.com/MineralsCloud/QuantumESPRESSO.jl/blob/{commit}{path}#{line}",
+    sitename = "QuantumESPRESSO.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://MineralsCloud.github.io/QuantumESPRESSO.jl",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/MineralsCloud/QuantumESPRESSO.jl",
-)
+deploydocs(; repo = "github.com/MineralsCloud/QuantumESPRESSO.jl")
