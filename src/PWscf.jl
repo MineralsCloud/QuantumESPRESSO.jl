690eb4e14cd96fd11466cf5a01be8ea777fba8b1
singularitti <singularitti@outlook.com>
2023-03-21 02:13:47 -0400

    Add src/PWscf.jl

---
+++ src/PWscf.jl
@@ -0,0 +1,9 @@
+module PWscf
+
+using Reexport: @reexport
+
+@reexport using QuantumESPRESSOBase.PWscf
+@reexport using QuantumESPRESSOParser.PWscf
+@reexport using QuantumESPRESSOFormatter.PWscf
+
+end
